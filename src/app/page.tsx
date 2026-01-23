d22c5702d9dfaf3fc278389111f9d888f089f1c7
elpeakyblinder <charly61666666@gmail.com>
2026-01-23 16:11:09 -0600

    feat: Add logo and improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 import { ArrowDown } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <div className="flex flex-col items-center gap-4">
-      <h1 className="text-4xl uppercase font-bold tracking-widest">
-        DailyFlow
-      </h1>
-      <span className="text-chart-3 text-2xl tracking-widest">
-        Genera tus reportes, gestiona los reportes de tu equipo.
-      </span>
-      <span>
-        <ArrowDown />
-      </span>
-      <Link href="/auth/login" className="text-primary underline">
-        Acceder
-      </Link>
+        <Image src="/logo/completedLogoDailyFlow.webp" alt="DailyFlow Logo" width={500} height={500} />
+        <h1 className="text-4xl uppercase font-bold tracking-widest hidden">
+          DailyFlow
+        </h1>
+        <span className="text-chart-3 text-2xl tracking-widest mt-2">
+          Genera tus reportes, gestiona los reportes de tu equipo.
+        </span>
+        <span>
+          <ArrowDown />
+        </span>
+        <Link href="/auth/login" className="text-primary underline">
+          Acceder
+        </Link>
       </div>
     </main>
   );
