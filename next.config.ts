8816750fec1b1993badfe5f3d9e38b8b44dfcc84
elpeakyblinder <charly61666666@gmail.com>
2026-01-26 19:55:44 -0600

    chore: View images vercel blob

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '2vjlrdbftsxsitev.public.blob.vercel-storage.com',
+        port: '',
+      },
+
+    ],
+  },
 };
 
 export default nextConfig;
